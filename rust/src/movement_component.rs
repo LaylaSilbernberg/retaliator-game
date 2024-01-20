63cb176d6c61023925fde3fe54a79ddb2c302193
Layla Silbernberg <119636966+LaylaSilbernberg@users.noreply.github.com>
2024-01-20 03:37:33 +0100

    further work on the player

--- rust/src/movement_component.rs
+++ rust/src/movement_component.rs
@@ -1,19 +1,43 @@
-use godot::engine::Resource;
+use godot::engine::Node3D;
+use godot::obj::WithBaseField;
 use godot::prelude::*;
 
 #[derive(GodotClass)]
-#[class(base=Resource)]
+#[class(base = Node3D, init)]
 pub struct MovementComponent {
     #[export]
     speed: f64,
     #[base]
-    base: Base<Resource>,
+    base: Base<Node3D>,
 }
+#[godot_api]
 impl MovementComponent {
-    const GLOBAL: Node3D = get_node("/root/PlayerVariables");
-    let vector: Vector3 = Vector3::new(0.0, 0.0, 0.0);
-
-    fn get_input(){
-
+    #[func]
+    fn get_input(&mut self) -> Vector3 {
+        let mut input_dir: Vector3 = Vector3 {
+            x: 0.0,
+            y: 0.0,
+            z: 0.0,
+        };
+        let mut velocity: Vector3 = Vector3 {
+            x: 0.0,
+            y: 0.0,
+            z: 0.0,
+        };
+        let global_transform = self.to_gd().get_global_transform().basis;
+        let input: Gd<Input> = Input::singleton();
+        if input.is_action_pressed("move_forward".into()) {
+            input_dir += -global_transform.col_c();
+        }
+        if input.is_action_pressed("move_backward".into()) {
+            input_dir += global_transform.col_c();
+        }
+        if input.is_action_pressed("strafe_left".into()) {
+            input_dir += -global_transform.col_a();
+        }
+        if input.is_action_pressed("strage_right".into()) {
+            input_dir += global_transform.col_a();
+        }
+        input_dir.normalized()
     }
 }
--- rust/src/player.rs
+++ rust/src/player.rs
@@ -1,12 +1,14 @@
-use godot::engine::CharacterBody3d;
+use godot::engine::CharacterBody3D;
 use godot::prelude::*;
 
+use crate::health_component::HealthComponent;
+use crate::movement_component::MovementComponent;
+
 #[derive(GodotClass)]
-#[class(base=CharacterBody3d)]
+#[class(base=CharacterBody3D)]
 pub struct Prayer {
-    #[export]
     health_component: HealthComponent,
     movement_component: MovementComponent,
     #[base]
-    base: Base<CharacterBody3d>,
+    base: Base<CharacterBody3D>,
 }
